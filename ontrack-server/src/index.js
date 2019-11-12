c16db5f37efcf15c5820e70f6d25102e914ece76
Luke Mainwaring <lukemainwaring@Lukes-MacBook-Pro-3.local>
2019-11-12 16:56:14 -0500

    attempt at full dockerization, externalize routes and db config

--- ontrack-server/src/index.js
+++ ontrack-server/src/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(authRoutes);
+
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello world!!\n');
 });
 
 app.listen(PORT, HOST);
